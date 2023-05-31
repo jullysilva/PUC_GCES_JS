97f4e37c126777984fa8fe95e645ea5260c38c8c
Jully Silva <jullyksilva@hotmail.com>
2023-05-30 21:51:12 -0300

    [TEST]: refazendo testes unitarios do react web

--- library_front/src/App.test.js
+++ library_front/src/App.test.js
@@ -1,3 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders perfil link to profile of user', () => {
+  render(<App />);
+  const linkElement = screen.getByText("Perfil");
+  expect(linkElement).toBeInTheDocument();
+});
+
 test('soma dois números', () => {
   const resultado = 2 + 3;
   expect(resultado).toBe(5);
